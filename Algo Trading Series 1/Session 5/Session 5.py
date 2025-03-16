914db66fd42bb60ed6ed4f7d1058999af096b9fa
Priya-Bai-S21 <163130934+Priya-Bai-S21@users.noreply.github.com>
2025-03-16 16:02:21 +0530

    Add files via upload

---
+++ Algo Trading Series 1/Session 5/Session 5.py
@@ -0,0 +1,22 @@
+
+watchlist = ["ACC", "CIPLA", "GAIL", "AXISBANK", "ZEEL", "LT"]
+
+
+for stock_name in watchlist:
+	print("scanning", stock_name)
+	print("aa")
+	print()
+
+
+
+
+for number in range(10):
+	print(number)
+
+
+
+
+
+
+
+
